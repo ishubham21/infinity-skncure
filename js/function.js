82ae26f2f5aefb4e472e1826e62bbce1a440a987
Shubham Gautam <sg2199203@gmail.com>
2021-03-20 12:41:36 +0530

    Added files

---
+++ js/function.js
@@ -0,0 +1,7 @@
+var preLoader = document.querySelector('.pre-loader')
+
+window.addEventListener('load', () => {
+    preLoader.classList.add('hide-loader')
+    $('.selectFile-container').hide()
+})
+    
